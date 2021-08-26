2ed38791cde2bc31d51080f5f67d4b42a289f49f
Alexandra N. Ofori-Adusei <89520838+xandra1@users.noreply.github.com>
2021-08-26 16:24:06 +0000

    Even and Odd numbers
    This code snippets checks and print whether a number is even or odd

---
+++ if-else/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int number;
+
+   cin >> number;
+
+   /* Your solution goes here  */
+   if (number % 2 == 0) {
+       cout << "Even";
+   }
+   else  {
+       cout << "Odd";
+   }
+
+   return 0;
+}
+
+
