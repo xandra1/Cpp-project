e79a42b31f12f2a9f1efe09072667b02f2495258
Alexandra N. Ofori-Adusei <89520838+xandra1@users.noreply.github.com>
2021-08-27 17:59:43 +0000

    Uploading Project Files
    ____
    Write a computer program that enables a user to
    1.Create a user accounts
      _Accounts Requirement_
    #first and last name
    #username
    #password
    #gender
    #email
    #birth date
    #Age
    #Nationality
    2. Anyone below 18 years isn't qualified to create this account
    3. The accounts is for Americans only (people from other countries cannot create the accounts)
    4.Only the username and password is required to log in
    5. After the accounts has been created, the program should display a verification message ( Account created Successfully)
    6. After the verification message, the program should then pause for 5 seconds
    7. After the pause, the user should then be given the opportunity to log in with his Credentials
    8. The final output is the user's personal information( Which was entered earlier ie. Accounts Requirements )

---
+++ Login System/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    string path = "C:/Users/User/Documents/Personal/Documents/Lectures/SEM2/DCIT104/Lab Sessions/alexa/users/";
+   string firstname;
+   string lastname;
+   string username;
+   string password;
+   string gender;
+   string email;
+   string birthDate;
+   int age;
+   string nationality;
+
+
+   cout << "Enter first name: ";
+   cin >> firstname;
+   cout << "Enter last name: ";
+   cin >> lastname;
+   cout << "Enter username: ";
+   cin >> username;
+   cout << "Enter  password: ";
+   cin >> password;
+   cout << "Gender: ";
+   cin >> gender;
+   cout << "Enter email: ";
+   cin >> email;
+   cout << "Enter date of birth: ";
+   cin >> birthDate;
+   cout << "Enter your age: ";
+   cin >> age;
+   cout << "Enter your nationality: ";
+   cin >> nationality;
+
+
+
+
+    //if user age is below 18
+    if ( age < 18 ){
+        cout << "Sorry your age does not qualified to register."<< endl;
+    }else if(nationality != "America"){
+        cout << "Sorry your nationality does not qualified to register."<< endl;
+    }else{
+    //creating file to store data
+       ofstream MyFile(path+username+".txt");
+       //writing user data to username.txt
+        MyFile << username << endl;
+        MyFile << password << endl;
+        MyFile << firstname << endl;
+        MyFile << lastname << endl;
+        MyFile << email << endl;
+        MyFile << gender << endl;
+        MyFile << birthDate << endl;
+        MyFile << age << endl;
+        MyFile << nationality << endl;
+
+        cout << "Account created successsfully" <<endl;
+        MyFile.close();
+    }
+
+
+    return 0;
+}
