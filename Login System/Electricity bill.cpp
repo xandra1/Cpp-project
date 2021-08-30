b6ac071925753a346f2d5cb5c14a83a6b746a257
Alexandra N. Ofori-Adusei <89520838+xandra1@users.noreply.github.com>
2021-08-30 13:57:36 +0000

    Create Electricity bill.cpp
    Calculate the amount of money to be paid based on certain scenarios.
    
    After the announcement by the government of its intention to pay 50% of the electricity bill of all categories of consumer, the board of Electricity Company of Ghana (ECG) were in a dilemma as to whether to adjust the old billing software or design a new one. It was decided that a new software should be designed because the old one was designed using FORTRAN and there are no FORTRAN programmers around. Each time they require to make changes to the software, they had to invite the designers from US, this is quite expensive than building a new one locally. You have won a contract to design the software using c++. On a trial basis you are to design a console application that will
    
    a) request for i. customer name ii quantity of electricity used iii categorize customers into
    
    domestic when at most 150 KW units is used within
    industrial when more than 150 kw but less than or equal to 350 KW is used
    commercial when more than 350 KW is used (8 marks)CR
    b) if the cost per KW for domestic, industrial and commercial are respectively Ghc 1.2, 2.3 and 3.5 respectively. Your program should
    
    compute the monthly bill based on category.
    Add 1% of the monthly bill for rural electrification to get the total bill
    reduce the total bill by 50% (8 marks) EV
    c) Display the following results
    
    customer’s name
    quantity of water consumed and category
    1% rural electrification charge and total amount for the month
    50% government subsidy
    balance to be paid (4 marks) CR NB: use the appropriate data type for all the variables

---
+++ Login System/Electricity bill.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string customerName;
+
+    float  quantityOfElectricity;
+
+    string category;
+
+    double monthlyBill;
+
+
+
+    cout << "Customer's Name: ";
+
+    getline(cin, customerName);
+
+    cout << "Quantity of electricity consumed:  ";
+
+    cin >> quantityOfElectricity;
+
+    if (quantityOfElectricity <= 150){
+
+        category = "Domestic";
+        monthlyBill = quantityOfElectricity * 1.2;
+
+    }else if (quantityOfElectricity > 350){
+
+        category = "Commercial";
+        monthlyBill = quantityOfElectricity * 3.5;
+    }else{
+        category = "Industrial";
+        monthlyBill = quantityOfElectricity * 2.3;
+    }
+
+
+
+
+   double ruralElectrification = monthlyBill * 0.01;
+
+     monthlyBill = monthlyBill + ruralElectrification;
+
+   double government_subsidy = 0.5 * monthlyBill;
+
+     double payable_amount = monthlyBill - government_subsidy;
+
+
+
+
+        cout << "Name: " << customerName << endl;
+
+        cout << "Quantity of Electricity Consumed: " << quantityOfElectricity << endl;
+
+        cout << "Category: " << category << endl;
+
+        cout << "1% Rural Electrification charge: " << ruralElectrification << endl;
+
+        cout << "50% Government subsidy: " << government_subsidy << endl;
+
+       cout << "Total amount for the month: " << payable_amount << endl;
+
+
+
+
+
+
+
+
+        return 0;
+}
