fcd46e9a387ecd65d8983165ea6c1b9084cff4d3
Alexandra N. Ofori-Adusei <89520838+xandra1@users.noreply.github.com>
2021-08-31 10:27:34 +0000

    ...
    A cashier distributes change using the maximum number of five dollar bills, followed by one dollar bills. For example, 19 yields 3 fives and 4 ones.

---
+++ Change.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+   
+  
+  
+  int amountToChange;
+  
+  int numFives;
+  
+  int numOnes;
+    
+  
+    cin >> amountToChange;
+
+    numFives = amountToChange / 5;
+
+      int amountLeft = amountToChange % 5;
+
+      numOnes = amountLeft / 1;
+
+
+     cout << "numFives: " << numFives << endl;
+     cout << "numOnes: " << numOnes << endl;
+
+
+
+    return 0;
+}
