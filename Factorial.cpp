ba20abb218aa2a7142991842a3fc004b84d2ec21
Alexandra N. Ofori-Adusei <89520838+xandra1@users.noreply.github.com>
2021-09-01 15:36:33 +0000

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int isfactorial(int n){
+    if( n ==1){
+        return n;
+        
+    }else{
+        return isfactorial(n-1)*n;
+    }
+
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "Factorial of " << n << " is " << isfactorial(n)<< endl;
+
+    return 0;
+}
+
