34152afa2fca3e59e12d622a4a7bd05ca2d96318
Alexandra N. Ofori-Adusei <89520838+xandra1@users.noreply.github.com>
2021-10-11 17:27:48 +0000

    Add files via upload

---
+++ loan/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int input;
+    int payment1;
+    int payment2;
+    int payment3;
+    int output;
+    cout << "Enter amount of loan: ";
+    cin >> input;
+
+    //monthly payment schedule//
+    payment1 = input * 0.1;
+
+
+    cout << payment1;
+
+    return 0;
+}
