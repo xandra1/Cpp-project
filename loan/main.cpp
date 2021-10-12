d1238eb24870a7c87694d8ff674855d121ca5e9b
Alexandra N. Ofori-Adusei <89520838+xandra1@users.noreply.github.com>
2021-10-12 15:04:22 +0000

    Update main.cpp

--- loan/main.cpp
+++ loan/main.cpp
@@ -9,15 +9,24 @@ int main()
     int payment1;
     int payment2;
     int payment3;
+    int remainder;
     int output;
+
     cout << "Enter amount of loan: ";
     cin >> input;
 
     //monthly payment schedule//
     payment1 = input * 0.1;
+    remainder= input - payment1;
 
+    payment2= remainder * 0.1;
+    remainder= remainder - payment2;
 
-    cout << payment1;
+    payment3= remainder * 0.1;
+    remainder= remainder - payment3;
+
+    output = remainder;
+    cout << output;
 
     return 0;
 }
