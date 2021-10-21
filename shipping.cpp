e3e84bcca40c1a1544ae7f482852e46762519253
Alexandra N. Ofori-Adusei <89520838+xandra1@users.noreply.github.com>
2021-10-21 20:03:55 +0000

    Update shipping.cpp

--- shipping.cpp
+++ shipping.cpp
@@ -5,11 +5,8 @@ using namespace std;
 int main() {
 
    int shipWeightPounds;
-
    int shipCostCents = 0;
-
    const int FLAT_FEE_CENTS = 75;
-
    const int CENTS_PER_POUND = 25;
    
    //Requesting for shipping weight
