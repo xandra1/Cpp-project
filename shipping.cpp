31c4fae47dc10ef79b1f0e2ee84f69b30946d2fb
Alexandra N. Ofori-Adusei <89520838+xandra1@users.noreply.github.com>
2021-10-30 17:04:52 +0000

    Update shipping.cpp

--- shipping.cpp
+++ shipping.cpp
@@ -8,18 +8,15 @@ int main() {
    int shipCostCents = 0;
    const int FLAT_FEE_CENTS = 75;
    const int CENTS_PER_POUND = 25;
-   
    //Requesting for shipping weight
    cout << "Enter Shipping Weight: ";
    cin >> shipWeightPounds;   //Accepting weight of item(lbs) from user into shipWeightPounds variable
    shipCostCents = (CENTS_PER_POUND * shipWeightPounds) + FLAT_FEE_CENTS; //Calculating total cost_per_cent
-   
    //Final Output
    cout << "Weight(lb): " << shipWeightPounds;
    cout << ", Flat fee(cents): " << FLAT_FEE_CENTS;
    cout << ", Cents per lb: " << CENTS_PER_POUND << endl;
-   cout << "Shipping cost(cents): " << shipCostCents << endl;
-
+   cout << "Shipping cost(cents): " << shipCostCents << endl
    return 0;
 
 }
